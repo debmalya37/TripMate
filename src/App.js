3dcc400e4f1aa8f008039c9d4a5b8d3355ca419c
Debmalya <debmalyasen37@gmail.com>
2023-07-15 16:01:23 +0530

    #1 Initial Files ADD: added all files and updated off github issues and made it a 0th version and working on it

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Swipe from "./Components/Swipe";
+
+function App() {
+  return (
+    <div className="App">
+      <Swipe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Data.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    name: "John",
+    picture:
+      "https://life-to-go.com/wp-content/uploads/2016/05/Nungnung-Wasserfall-Daniel-Jessica-683x1024.jpg",
+    bio: "I love hiking and photography.",
+  },
+  {
+    name: "Emily",
+    picture:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBrDgyv3XmBLBnVyX6_Dct4moaL9d-D1Nl3582xz9HG5zY-H-k-yoSVOFc7BSYKXADnE&usqp=CAU",
+    bio: "Passionate about painting and traveling.",
+  },
+  {
+    name: "David",
+    picture:
+      "https://img.freepik.com/premium-photo/freedom-adventure-travel-lifestyle-adult-couple-nature-woods-forest-standing-woman-look-outdoor-park-background-drink-man-lay-down-hammock-rest_425263-5630.jpg?w=2000",
+    bio: "Music enthusiast and book lover.",
+  },
+  {
+    name: "Sarah",
+    picture:
+      "https://i.pinimg.com/originals/a0/cb/2e/a0cb2ef2793a99ddf44c8d037328859e.jpg",
+    bio: "Fitness enthusiast and foodie.",
+  },
+];
